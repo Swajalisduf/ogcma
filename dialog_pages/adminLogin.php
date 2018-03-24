91beac48a00593ffa9b1714b8ecd4b8ca984937e
Mike Kim <mikeorsomething@gmail.com>
2018-03-23 22:35:41 -0600

    Moved admin login to dialog_pages folder to clean up code on admin.php file and allow us to include it in other files if need be

---
+++ dialog_pages/adminLogin.php
@@ -0,0 +1,18 @@
+<div id="login_box">
+	<div id="login_content">
+		<form id="login_form" method="post">
+		<h1>Login</h1>
+		<label>Username</label>
+				<input type="text" name="username" value="" placeholder="Your Username">
+		<label>Password</label>
+				<input type="password" name="password" value="" placeholder="Your password">			
+		<button class="submit">Login</button>
+		</form>
+		
+			<a href="">Forgot your password?</a>
+			<p>Do not have an account?</p>
+			<a href="registration.php?page=registration">Create an Account</a>
+			<div id="error">
+			</div>
+	</div>
+</div>
