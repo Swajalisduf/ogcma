76874f64b1310b33a49f37ef46c0fba618bc81da
Mike Kim <mikeorsomething@gmail.com>
2018-05-02 08:25:37 -0600

    Added menu-slide animations

--- js/index/0_index.js
+++ js/index/0_index.js
@@ -10,6 +10,7 @@ const userEvents = {
 		e.preventDefault();
 		let line1 = document.querySelector('.line1');
 		let line3 = document.querySelector('.line3');
+		let ul = document.querySelector('nav ul');
 		
 		if(userEvents.clicked) {
       userEvents.clicked = false;
@@ -17,12 +18,16 @@ const userEvents = {
       line1.classList.add('line1-forward');
       line3.classList.remove('line3-reverse');
       line3.classList.add('line3-forward');
+      ul.classList.remove('menu-up');
+      ul.classList.add('menu-down');
     } else {
     	userEvents.clicked = true;
     	line1.classList.remove('line1-forward');
       line1.classList.add('line1-reverse');
       line3.classList.remove('line3-forward');
       line3.classList.add('line3-reverse');
+      ul.classList.remove('menu-down');
+      ul.classList.add('menu-up');
    	} 
 	}
 }
