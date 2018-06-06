2ce03aece904e8f06a52637e08309e0f297f5d84
Michelle Bluth <mmbluth4@gmail.com>
2018-06-06 10:09:09 -0600

    fixed the alignment of the tags

--- browse.php
+++ browse.php
@@ -9,47 +9,47 @@
 	$usages = $read->getEntries();
 ?>
 
-<main>
-	<div id="container2">
+<div id="container2">
+	<main>
 		<img class='top-image' src='images/background_top_1300px.png' alt='background'>
 		<div>
 			<h1>Welcome to the Catalog of all the References</h1>
 			<input class="allSearch" type="text" name="allSearch" placeholder="Search All Usages">
 		</div>
-				<div>
-					<div class="sort">
-						<ul class="clearfix">
-							<li>A</li>
-							<li>B</li>
-							<li>C</li>
-							<li>D</li>
-							<li>E</li>
-							<li>F</li>
-							<li>G</li>
-							<li>H</li>
-							<li>I</li>
-							<li>J</li>
-							<li>K</li>
-							<li>L</li>
-							<li>M</li>
-							<li>N</li>
-							<li>O</li>
-							<li>P</li>
-							<li>Q</li>
-							<li>R</li>
-							<li>S</li>
-							<li>T</li>
-							<li>U</li>
-							<li>V</li>
-							<li>W</li>
-							<li>X</li>
-							<li>Y</li>
-							<li>Z</li>
-						</ul>
-					</div>
+			<div>
+				<div class="sort">
+					<ul class="clearfix">
+						<li>A</li>
+						<li>B</li>
+						<li>C</li>
+						<li>D</li>
+						<li>E</li>
+						<li>F</li>
+						<li>G</li>
+						<li>H</li>
+						<li>I</li>
+						<li>J</li>
+						<li>K</li>
+						<li>L</li>
+						<li>M</li>
+						<li>N</li>
+						<li>O</li>
+						<li>P</li>
+						<li>Q</li>
+						<li>R</li>
+						<li>S</li>
+						<li>T</li>
+						<li>U</li>
+						<li>V</li>
+						<li>W</li>
+						<li>X</li>
+						<li>Y</li>
+						<li>Z</li>
+					</ul>
 				</div>
-	<div>
-</main>
+			</div>
+	</main>
+</div>
 
 <?php
 	require_once 'includes/partials/footer.php';
