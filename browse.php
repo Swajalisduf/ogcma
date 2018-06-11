949bc893e834c5e9d6c66c2baf4d1694a1337cf4
Michelle Bluth <mmbluth4@gmail.com>
2018-06-11 15:32:51 -0600

    still testing

--- browse.php
+++ browse.php
@@ -11,44 +11,40 @@
 
 
 <main>
-	<div id="container2">
-		<img class='top-image' src='images/background_top_1300px.png' alt='background'>
-		<div>
-			<h1>Welcome to the Catalog of all the References</h1>
-			<input class="allSearch" type="text" name="allSearch" placeholder="Search All Usages">
-		</div>
-			<div>
-				<div class="sort">
-					<ul class="clearfix">
-						<li>A</li>
-						<li>B</li>
-						<li>C</li>
-						<li>D</li>
-						<li>E</li>
-						<li>F</li>
-						<li>G</li>
-						<li>H</li>
-						<li>I</li>
-						<li>J</li>
-						<li>K</li>
-						<li>L</li>
-						<li>M</li>
-						<li>N</li>
-						<li>O</li>
-						<li>P</li>
-						<li>Q</li>
-						<li>R</li>
-						<li>S</li>
-						<li>T</li>
-						<li>U</li>
-						<li>V</li>
-						<li>W</li>
-						<li>X</li>
-						<li>Y</li>
-						<li>Z</li>
-					</ul>
-				</div>
-			</div>
+	<img class='top-image' src='images/background_top_1300px.png' alt='background'>
+	<div>
+		<h1>Welcome to the Catalog of all the References</h1>
+		<input class="allSearch" type="text" name="allSearch" placeholder="Search All Usages">
+	</div>
+	<div class="sort">
+		<ul class="clearfix">
+			<li>A</li>
+			<li>B</li>
+			<li>C</li>
+			<li>D</li>
+			<li>E</li>
+			<li>F</li>
+			<li>G</li>
+			<li>H</li>
+			<li>I</li>
+			<li>J</li>
+			<li>K</li>
+			<li>L</li>
+			<li>M</li>
+			<li>N</li>
+			<li>O</li>
+			<li>P</li>
+			<li>Q</li>
+			<li>R</li>
+			<li>S</li>
+			<li>T</li>
+			<li>U</li>
+			<li>V</li>
+			<li>W</li>
+			<li>X</li>
+			<li>Y</li>
+			<li>Z</li>
+		</ul>
 	</div>
 </main>
 
