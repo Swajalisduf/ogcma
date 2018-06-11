abd1db2caf2407bb0af5e35f714e5697049a9df3
Michelle Bluth <mmbluth4@gmail.com>
2018-06-11 15:54:03 -0600

    test

--- browse.php
+++ browse.php
@@ -15,36 +15,36 @@
 	<div>
 		<h1>Welcome to the Catalog of all the References</h1>
 		<input class="allSearch" type="text" name="allSearch" placeholder="Search All Usages">
-	</div>
-	<div class="sort">
-		<ul class="clearfix">
-			<li>A</li>
-			<li>B</li>
-			<li>C</li>
-			<li>D</li>
-			<li>E</li>
-			<li>F</li>
-			<li>G</li>
-			<li>H</li>
-			<li>I</li>
-			<li>J</li>
-			<li>K</li>
-			<li>L</li>
-			<li>M</li>
-			<li>N</li>
-			<li>O</li>
-			<li>P</li>
-			<li>Q</li>
-			<li>R</li>
-			<li>S</li>
-			<li>T</li>
-			<li>U</li>
-			<li>V</li>
-			<li>W</li>
-			<li>X</li>
-			<li>Y</li>
-			<li>Z</li>
-		</ul>
+		<div class="sort">
+			<ul class="clearfix">
+				<li>A</li>
+				<li>B</li>
+				<li>C</li>
+				<li>D</li>
+				<li>E</li>
+				<li>F</li>
+				<li>G</li>
+				<li>H</li>
+				<li>I</li>
+				<li>J</li>
+				<li>K</li>
+				<li>L</li>
+				<li>M</li>
+				<li>N</li>
+				<li>O</li>
+				<li>P</li>
+				<li>Q</li>
+				<li>R</li>
+				<li>S</li>
+				<li>T</li>
+				<li>U</li>
+				<li>V</li>
+				<li>W</li>
+				<li>X</li>
+				<li>Y</li>
+				<li>Z</li>
+			</ul>
+		</div>
 	</div>
 </main>
 
